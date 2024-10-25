e9fcb0b195b2275906a01ba061cbc3e255b1823e
Finn2400 <66322386+Finn2400@users.noreply.github.com>
2024-10-25 14:28:07 -0500

    allow importing of all_chain_rmsx

--- rmsx/__init__.py
+++ rmsx/__init__.py
@@ -1,6 +1,7 @@
 # rmsx/rmsx/__init__.py
 
-from .rmsx import run_rmsx, combine_pdb_files, plot_rmsx_surface
+from .rmsx import run_rmsx, plot_rmsx_surface, all_chain_rmsx
+
 # Import other necessary functions or classes if any
 
 __all__ = ['run_rmsx']
