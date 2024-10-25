c389bf6336d0fcf486bc739afe8225638503ceb2
Finn2400 <66322386+Finn2400@users.noreply.github.com>
2024-10-25 12:26:54 -0500

    Testing all_chain_rmsx() to allow running of all the chains at once.

--- rmsx/__init__.py
+++ rmsx/__init__.py
@@ -1,6 +1,6 @@
 # rmsx/rmsx/__init__.py
 
-from .rmsx import run_rmsx
+from .rmsx import run_rmsx, combine_pdb_files, plot_rmsx_surface
 # Import other necessary functions or classes if any
 
 __all__ = ['run_rmsx']
