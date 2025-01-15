0780d85e945cfa6dbf8151fcc79a5f158deef037
Finn2400 <66322386+Finn2400@users.noreply.github.com>
2025-01-15 13:04:43 -0600

    added check to make sure the slice sizes were always the same when user sets the number of slices for RMSX

--- rmsx/__init__.py
+++ rmsx/__init__.py
@@ -6,4 +6,11 @@ from .flipbook import run_flipbook  # Add this line to import run_flipbook
 
 # updated to include flipbook option
 # Update __all__ to include run_flipbook
-__all__ = ['run_rmsx', 'run_flipbook']
+__all__ = [
+    'run_rmsx',
+    'combine_pdb_files',
+    'plot_rmsx_surface',
+    'all_chain_rmsx',
+    'run_rmsx_flipbook',
+    'run_flipbook'
+]
